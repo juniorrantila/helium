23cd52b5a19550bf13b3466bb3e71dae745b80d5
Junior Rantila <junior.rantila@gmail.com>
2022-09-14 22:45:00 +0200

    Core: Make ArgumentParser use ids

--- subprojects/Core/CLI/ArgumentParser.h
+++ subprojects/Core/CLI/ArgumentParser.h
@@ -1,34 +1,44 @@
 #pragma once
+#include <Types.h>
 #include <functional>
 #include <iostream>
 #include <string_view>
+#include <map>
 
 namespace CLI {
 
 class ArgumentParser {
 public:
     void add_flag(std::string_view long_name,
-                  std::string_view short_name,
-                  std::string_view explanation,
-                  std::function<void()>&& callback)
+        std::string_view short_name, std::string_view explanation,
+        std::function<void()> const& callback)
     {
         flags.push_back({ long_name, short_name, explanation });
-        long_flag_callbacks[long_name] = callback;
-        short_flag_callbacks[short_name] = callback;
+        auto id = flag_callbacks.size();
+        flag_callbacks.push_back(callback);
+        long_flag_callback_ids[long_name] = id;
+        short_flag_callback_ids[short_name] = id;
     }
 
     void add_option(std::string_view long_name,
-                    std::string_view short_name,
-                    std::string_view placeholder,
-                    std::string_view explanation,
-                    std::function<void(std::string_view)>&& callback)
+        std::string_view short_name, std::string_view placeholder,
+        std::string_view explanation,
+        std::function<void(c_string)> const& callback)
     {
-        options.push_back({ long_name, short_name, explanation, placeholder });
-        long_option_callbacks[long_name] = callback;
-        short_option_callbacks[short_name] = callback;
+        options.push_back({
+            long_name,
+            short_name,
+            explanation,
+            placeholder,
+        });
+        auto id = option_callbacks.size();
+        option_callbacks.push_back(callback);
+        long_option_callback_ids[long_name] = id;
+        short_option_callback_ids[short_name] = id;
     }
 
-    void add_positional_argument(std::string_view placeholder, std::function<void(std::string_view)>&& callback)
+    void add_positional_argument(std::string_view placeholder,
+        std::function<void(c_string)> const& callback)
     {
         positional_argument_placeholders.push_back(placeholder);
         positional_argument_callbacks.push_back(callback);
@@ -36,7 +46,8 @@ public:
 
     void run(int argc, char const* argv[]) const;
 
-    void print_usage_and_exit(std::string_view program_name, int exit_code = 0) const;
+    void print_usage_and_exit(std::string_view program_name,
+        int exit_code = 0) const;
 
 private:
     struct Flag {
@@ -54,13 +65,20 @@ private:
     };
     std::vector<Option> options {};
 
-    std::unordered_map<std::string_view, std::function<void()>> short_flag_callbacks {};
-    std::unordered_map<std::string_view, std::function<void()>> long_flag_callbacks {};
-    std::unordered_map<std::string_view, std::function<void(std::string_view)>> short_option_callbacks {};
-    std::unordered_map<std::string_view, std::function<void(std::string_view)>> long_option_callbacks {};
+    std::map<std::string_view, u32> short_flag_callback_ids {};
+    std::map<std::string_view, u32> long_flag_callback_ids {};
 
-    std::vector<std::string_view> positional_argument_placeholders {};
-    std::vector<std::function<void(std::string_view)>> positional_argument_callbacks {};
+    std::map<std::string_view, u32> short_option_callback_ids {};
+    std::map<std::string_view, u32> long_option_callback_ids {};
+
+    std::vector<std::function<void()>> flag_callbacks;
+    std::vector<std::function<void(c_string)>>
+        option_callbacks;
+
+    std::vector<std::string_view>
+        positional_argument_placeholders {};
+    std::vector<std::function<void(c_string)>>
+        positional_argument_callbacks {};
 };
 
 }
