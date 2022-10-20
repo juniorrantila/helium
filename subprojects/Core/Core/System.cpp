86fda007c67a682224d03531e8ad5762c67c8b36
Junior Rantila <junior.rantila@gmail.com>
2022-10-20 20:18:16 +0200

    Core: Add System - a library wrapping syscalls and the like

---
+++ subprojects/Core/Core/System.cpp
@@ -0,0 +1,166 @@
+#include <Core/System.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+namespace Core::System {
+
+ErrorOr<void> fsync(int fd)
+{
+    if (::fsync(fd) < 0)
+        return Error::from_errno();
+    return {};
+}
+
+ErrorOr<Stat> fstat(int fd)
+{
+    struct stat st;
+    if (::fstat(fd, &st) < 0)
+        return Error::from_errno();
+    return Stat { .raw = st, };
+}
+
+ErrorOr<size_t> write(int fd, void const* data, size_t size)
+{
+    auto bytes = ::write(fd, data, size);
+    if (bytes < 0)
+        return Error::from_errno();
+    return bytes;
+}
+
+ErrorOr<size_t> write(int fd, StringView string)
+{
+    return TRY(write(fd, string.data, string.size));
+}
+
+ErrorOr<size_t> write(int fd, MappedFile const& file)
+{
+    return TRY(write(fd, file.m_data, file.m_size));
+}
+
+ErrorOr<size_t> write(int fd, StringBuffer const& string)
+{
+    return TRY(write(fd, string.data(), string.size()));
+}
+
+ErrorOr<size_t> writev(int fd, struct iovec const* iovec, int count)
+{
+    auto bytes_written = ::writev(fd, iovec, count);
+    if (bytes_written < 0)
+        return Error::from_errno();
+    return bytes_written;
+}
+
+ErrorOr<void> munmap(void const* addr, size_t size)
+{
+    if (::munmap((void*)addr, size) < 0)
+        return Error::from_errno();
+    return {};
+}
+
+ErrorOr<u8*> mmap(size_t size, int prot, int flags, int fd, off_t offset)
+{
+    return TRY(mmap(nullptr, size, prot, flags, fd, offset));
+}
+
+ErrorOr<u8*> mmap(void* addr, size_t size, int prot, int flags, int fd, off_t offset)
+{
+    auto* ptr = ::mmap(addr, size, prot, flags, fd, offset);
+    if (ptr == MAP_FAILED)
+        return Error::from_errno();
+    return (u8*)ptr;
+}
+
+ErrorOr<void> remove(c_string path)
+{
+    if (::remove(path) < 0)
+        return Error::from_errno();
+    return {};
+}
+
+ErrorOr<int> open(c_string path, int flags)
+{
+    if ((flags & O_CREAT) != 0) {
+        return Error::from_string_literal("O_CREAT should not be used with this function variant");
+    }
+    auto fd = ::open(path, flags);
+    if (fd < 0)
+        return Error::from_errno();
+    return fd;
+}
+
+ErrorOr<int> open(c_string path, int flags, mode_t mode)
+{
+    auto fd = ::open(path, flags | O_CREAT, mode);
+    if (fd < 0)
+        return Error::from_errno();
+    return fd;
+}
+
+ErrorOr<void> close(int fd)
+{
+    TRY(fsync(fd));
+    if (::close(fd) < 0)
+        return Error::from_errno();
+    return {};
+}
+
+ErrorOr<void> unlink(c_string path)
+{
+    if (::unlink(path) < 0)
+        return Error::from_errno();
+    return {};
+}
+
+ErrorOr<int> mkstemps(char* template_, int suffixlen)
+{
+    int fd = ::mkstemps(template_, suffixlen);
+    if (fd < 0)
+        return Error::from_errno();
+    return fd;
+}
+
+ErrorOr<int> mkstemps(char* template_)
+{
+    return TRY(mkstemps(template_, 0));
+}
+
+ErrorOr<pid_t> posix_spawnp(c_string file, c_string const* argv,
+    c_string const* envp,
+    posix_spawn_file_actions_t const* file_actions,
+    posix_spawnattr_t const* attrp)
+{
+    pid_t pid = -1;
+    auto rc = ::posix_spawnp(&pid, file, file_actions, attrp, (char**)argv, (char**)envp);
+    if (rc != 0)
+        return Error::from_errno(rc);
+    if (pid < 0)
+        return Error::from_errno();
+    return pid;
+}
+
+ErrorOr<Status> waitpid(pid_t pid, int options)
+{
+    int status = 0;
+    if (::waitpid(pid, &status, options) < 0)
+        return Error::from_errno();
+    return Status {
+        .raw = status
+    };
+}
+
+bool isatty(int fd)
+{
+    return ::isatty(fd) == 1;
+}
+
+ErrorOr<long> sysconf(int name)
+{
+    auto value = ::sysconf(name);
+    if (value == -1)
+        return Error::from_errno();
+    return value;
+}
+
+}
---
+++ subprojects/Core/Core/System.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <Core/ErrorOr.h>
+#include <Core/MappedFile.h>
+#include <Core/StringBuffer.h>
+#include <fcntl.h>
+#include <spawn.h>
+#include <unistd.h>
+#include <sys/uio.h>
+#include <sys/stat.h>
+
+#ifndef _GNU_SOURCE
+extern char** environ;
+#endif
+
+namespace Core::System {
+
+ErrorOr<size_t> write(int fd, MappedFile const& file);
+ErrorOr<size_t> write(int fd, StringBuffer const& string);
+ErrorOr<size_t> write(int fd, StringView string);
+ErrorOr<size_t> write(int fd, void const* data, size_t size);
+ErrorOr<size_t> writev(int fd, struct iovec const* iovec, int count);
+ErrorOr<void> fsync(int fd);
+ErrorOr<void> munmap(void const* addr, size_t size);
+ErrorOr<u8*> mmap(size_t size, int prot, int flags, int fd = -1, off_t offset = 0);
+ErrorOr<u8*> mmap(void* addr, size_t size, int prot, int flags, int fd = -1, off_t offset = 0);
+
+struct Stat {
+    constexpr bool is_regular() const { return S_ISREG(raw.st_mode); }
+    constexpr off_t size() const { return raw.st_size; }
+
+    struct stat raw;
+};
+ErrorOr<Stat> fstat(int fd);
+
+ErrorOr<int> mkstemps(char* template_);
+ErrorOr<int> mkstemps(char* template_, int suffixlen);
+ErrorOr<int> open(c_string path, int flags);
+ErrorOr<int> open(c_string path, int flags, mode_t mode);
+ErrorOr<void> close(int fd);
+ErrorOr<void> remove(c_string path);
+ErrorOr<void> unlink(c_string path);
+
+ErrorOr<pid_t> posix_spawnp(c_string file, c_string const* argv,
+    c_string const* envp = environ,
+    posix_spawn_file_actions_t const* file_actions = nullptr,
+    posix_spawnattr_t const* attrp = nullptr);
+
+struct Status {
+    constexpr bool did_exit() const { return WIFEXITED(raw); }
+    constexpr int exit_status() const { return WEXITSTATUS(raw); }
+
+    int raw;
+};
+ErrorOr<Status> waitpid(pid_t pid, int options = 0);
+
+bool isatty(int fd);
+
+ErrorOr<long> sysconf(int name);
+
+}
