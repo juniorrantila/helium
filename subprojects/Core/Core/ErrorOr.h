321e9e67f16138560947d25ea877a28bddc5e2fa
Junior Rantila <junior.rantila@gmail.com>
2022-10-20 21:15:20 +0200

    Core: Remove dependency on std::variant

--- subprojects/Core/Core/ErrorOr.h
+++ subprojects/Core/Core/ErrorOr.h
@@ -2,61 +2,94 @@
 #include <Core/Error.h>
 #include <optional>
 #include <type_traits>
-#include <variant>
 
 namespace Core {
 
 template <typename T, typename E = Error>
 struct [[nodiscard]] ErrorOr {
-    constexpr ErrorOr(T&& value)
-        requires(!std::is_trivially_copyable_v<T>)
-        : m_storage(std::move(value))
+    constexpr ErrorOr(ErrorOr&& other)
+        : m_state(other.m_state)
+    {
+        switch(m_state) {
+            case State::Error: m_error = other.release_error(); break;
+            case State::Value: m_value = other.release_value(); break;
+            case State::Moved: break;
+        }
+        other.m_state = State::Moved;
+    }
+
+    constexpr ErrorOr(T&& value) requires(
+        !std::is_trivially_copyable_v<T>)
+        : m_value(std::move(value))
+        , m_state(State::Value)
     {
     }
 
-    constexpr ErrorOr(T value)
-        requires(std::is_trivially_copyable_v<T>)
-        : m_storage(value)
+    constexpr ErrorOr(T value) requires(
+        std::is_trivially_copyable_v<T>)
+        : m_value(value)
+        , m_state(State::Value)
     {
     }
 
-    constexpr ErrorOr(E&& error)
-        requires(!std::is_trivially_copyable_v<E>)
-        : m_storage(std::move(error))
+    constexpr ErrorOr(E&& error) requires(
+        !std::is_trivially_copyable_v<E>)
+        : m_error(std::move(error))
+        , m_state(State::Error)
     {
     }
 
-    constexpr ErrorOr(E error)
-        requires(std::is_trivially_copyable_v<E>)
-        : m_storage(error)
+    constexpr ErrorOr(E error) requires(
+        std::is_trivially_copyable_v<E>)
+        : m_error(error)
+        , m_state(State::Error)
     {
     }
 
     template <typename EE>
-    constexpr ErrorOr(EE error)
-        requires(!std::is_same_v<E, Error> && std::is_constructible_v<E, EE>)
-        : m_storage(E(error))
+    constexpr ErrorOr(EE error) requires(
+        !std::is_same_v<E, Error> && std::is_constructible_v<E, EE>)
+        : m_error(E(error))
+        , m_state(State::Error)
     {
     }
 
-    constexpr bool is_error() const
+    constexpr ~ErrorOr()
     {
-        return m_storage.index() == 1;
+        switch (m_state) {
+            case State::Error: m_error.~E(); break;
+            case State::Value: m_value.~T(); break;
+            case State::Moved: break;
+        }
     }
 
-    T release_value() { return std::get<T>(std::move(m_storage)); }
+    constexpr bool is_error() const { return m_state == State::Error; }
 
-    E release_error() { return std::get<E>(std::move(m_storage)); }
-
-    T const& release_value() const
+    T release_value()
     {
-        return std::get<T>(m_storage);
+        m_state = State::Moved;
+        return std::move(m_value);
+    }
+    E release_error()
+    { 
+        m_state = State::Moved;
+        return std::move(m_error);
     }
 
-    E const& error() const { return std::get<E>(m_storage); }
+    T const& value() const { return m_value; }
+    E const& error() const { return m_error; }
 
 private:
-    std::variant<T, E> m_storage {};
+    union {
+        T m_value;
+        E m_error;
+    };
+    enum class State : u8 {
+        Value,
+        Error,
+        Moved,
+    };
+    State m_state;
 };
 
 template <typename E>
@@ -69,8 +102,8 @@ struct [[nodiscard]] ErrorOr<void, E> {
     }
 
     template <typename EE>
-    constexpr ErrorOr(EE error)
-        requires(!std::is_same_v<E, Error> && std::is_constructible_v<E, EE>)
+    constexpr ErrorOr(EE error) requires(
+        !std::is_same_v<E, Error> && std::is_constructible_v<E, EE>)
         : m_error(EE(error))
     {
     }
@@ -78,10 +111,9 @@ struct [[nodiscard]] ErrorOr<void, E> {
     constexpr bool is_error() const { return m_error.has_value(); }
 
     void release_value() const { }
+    E release_error() { return std::move(m_error.value()); }
 
-    E release_error() const { return std::move(*m_error); }
-
-    E const& error() const { return *m_error; }
+    E const& error() const { return m_error.value(); }
 
 private:
     std::optional<E> m_error {};
