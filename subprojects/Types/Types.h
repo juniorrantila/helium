5631c3063f382c90dbc1b50541cdfafe495686d3
Junior Rantila <junior.rantila@gmail.com>
2022-10-20 20:32:26 +0200

    Types: Add operator""sv()

--- subprojects/Types/Types.h
+++ subprojects/Types/Types.h
@@ -119,25 +119,58 @@ typedef struct StringView {
     {
     }
 
-    constexpr bool operator == (StringView other) const
+    constexpr bool operator==(StringView other) const
     {
         if (size != other.size)
             return false;
         if (data == other.data)
             return true;
-        return __builtin_strncmp(data, other.data, size) == 0;
-    }
-
-    constexpr bool operator == (char const* other) const
-    {
-        return *this == StringView(other);
+        bool same = true;
+        // clang-format off
+        while (other.size --> 0)
+            same &= data[other.size] == other.data[other.size];
+        // clang-format on
+        return same;
     }
 
     constexpr bool is_empty() const { return size == 0; }
 
-    constexpr char const& operator[](u32 index) const { return data[index]; }
+    constexpr char const& operator[](u32 index) const
+    {
+        return data[index];
+    }
+
+    constexpr u32 unchecked_copy_to(char* other, u32 size) const
+    {
+        if (data == other)
+            return size;
+        for (u32 i = 0; i < size; i++)
+            other[i] = data[i];
+        return size;
+    }
+
+    constexpr u32 unchecked_copy_to(char* __restrict other) const
+    {
+        return strncpy(other, *this);
+    }
+
+private:
+    static constexpr u32 strncpy(char* __restrict to, StringView from)
+    {
+        if (from.data == to)
+            return from.size;
+        for (u32 i = 0; i < from.size; i++)
+            to[i] = from[i];
+        return from.size;
+    }
 #endif
 } StringView;
+
+#if __cplusplus
+constexpr StringView operator ""sv(c_string data, size_t size) {
+    return StringView(data, size);
+}
+#endif
 
 #if __cplusplus
 #    include <iostream>
