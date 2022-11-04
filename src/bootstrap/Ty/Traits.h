fe109cf3189640ffb823062b3d07435a369761de
Junior Rantila <junior.rantila@gmail.com>
2022-11-04 08:27:14 +0100

    Ty: Add Traits

---
+++ src/bootstrap/Ty/Traits.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace Ty {
+
+template <typename T>
+inline constexpr bool is_trivial = __is_trivial(T);
+
+template <typename T>
+inline constexpr bool is_trivially_copyable = __is_trivially_copyable(T);
+
+template <typename T>
+inline constexpr bool is_trivially_destructible = __is_trivially_destructible(T);
+
+template <typename T, typename U>
+inline constexpr bool is_same = __is_same(T, U);
+
+template <typename T>
+inline constexpr bool is_const = false;
+
+template <typename T>
+inline constexpr bool is_const<T const> = true;
+
+}
+
+using namespace Ty;
