00b8c124ac8f0f5bb98dc86412918642b3178608
Junior Rantila <junior.rantila@gmail.com>
2022-11-06 13:35:10 +0100

    Ty: Add is_constant_evaluated()

--- src/bootstrap/Ty/Traits.h
+++ src/bootstrap/Ty/Traits.h
@@ -17,14 +17,16 @@ template <typename T, typename U>
 inline constexpr bool is_same = __is_same(T, U);
 
 template <typename T>
-inline constexpr bool is_const = false;
-
-template <typename T>
-inline constexpr bool is_const<T const> = true;
+inline constexpr bool is_const = __is_const(T);
 
 template <typename T, typename... Args>
 inline constexpr bool is_constructible
     = __is_constructible(T, Args...);
+
+inline constexpr bool is_constant_evaluated()
+{
+    return __builtin_is_constant_evaluated();
+}
 
 }
 
