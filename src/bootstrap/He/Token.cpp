c5c7cf271e76b02d00f6b2f398ec21a0ad01c69f
Junior Rantila <junior.rantila@gmail.com>
2022-11-13 17:46:40 +0100

    He: Use X macro trick in Token

--- src/bootstrap/He/Token.cpp
+++ src/bootstrap/He/Token.cpp
@@ -28,70 +28,10 @@ void Token::dump(StringView source) const
 StringView token_type_string(TokenType type)
 {
     switch (type) {
-#define CASE_RETURN(variant) \
-    case TokenType::variant: return #variant##sv
-        CASE_RETURN(OpenBracket);
-        CASE_RETURN(CloseBracket);
-
-        CASE_RETURN(OpenParen);
-        CASE_RETURN(CloseParen);
-
-        CASE_RETURN(OpenCurly);
-        CASE_RETURN(CloseCurly);
-
-        CASE_RETURN(Ampersand);
-        CASE_RETURN(Comma);
-        CASE_RETURN(Assign);
-        CASE_RETURN(NewLine);
-        CASE_RETURN(Number);
-        CASE_RETURN(Colon);
-        CASE_RETURN(Semicolon);
-        CASE_RETURN(Space);
-        CASE_RETURN(Hash);
-        CASE_RETURN(Underscore);
-        CASE_RETURN(QuestionMark);
-
-        CASE_RETURN(Minus);
-        CASE_RETURN(Plus);
-        CASE_RETURN(Slash);
-        CASE_RETURN(Star);
-
-        CASE_RETURN(Equals);
-        CASE_RETURN(GreaterThan);
-        CASE_RETURN(GreaterThanOrEqual);
-        CASE_RETURN(LessThan);
-        CASE_RETURN(LessThanOrEqual);
-
-        CASE_RETURN(Dot);
-        CASE_RETURN(Arrow);
-
-        CASE_RETURN(Quoted);
-        CASE_RETURN(Identifier);
-
-        CASE_RETURN(CFn);
-        CASE_RETURN(Fn);
-        CASE_RETURN(If);
-        CASE_RETURN(InlineC);
-        CASE_RETURN(Let);
-        CASE_RETURN(Pub);
-        CASE_RETURN(RefMut);
-        CASE_RETURN(Return);
-        CASE_RETURN(Var);
-        CASE_RETURN(While);
-
-        CASE_RETURN(Enum);
-        CASE_RETURN(Struct);
-        CASE_RETURN(Union);
-        CASE_RETURN(Variant);
-
-        CASE_RETURN(Embed);
-        CASE_RETURN(Import);
-        CASE_RETURN(ImportC);
-        CASE_RETURN(SizeOf);
-        CASE_RETURN(Uninitialized);
-
-        CASE_RETURN(Invalid);
-#undef CASE_RETURN
+#define X(variant, ...) \
+    case TokenType::variant: return #variant##sv;
+        TOKEN_TYPES
+#undef X
     }
 }
 
